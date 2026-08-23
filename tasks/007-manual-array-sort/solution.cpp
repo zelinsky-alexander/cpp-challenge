49a44c7761a501be57ba8efa9081b5d2d2032501
Alex Zelinsky <zelinsky.alexander@gmail.com>
2026-08-23 18:29:45 +0300

    done sortArray

--- tasks/007-manual-array-sort/solution.cpp
+++ tasks/007-manual-array-sort/solution.cpp
@@ -1,16 +1,33 @@
 #include <cassert>
 #include <vector>
+#include <iostream>
 
 void sortArray(std::vector<int>& values)
 {
+    const size_t sz = values.size();
+
+    if (sz <= 1) {
+        //std::cout << "nothing to sort, go away" << std::endl;
+        return;
+    }
+    
+    for (size_t i = 0; i < sz; ++i) {
+        size_t smallest = i;
+        for (size_t j = i + 1; j < sz; ++j) {
+            if (values[j] < values[smallest]) {
+                smallest = j;
+            }
+        }
+        std::swap(values[i], values[smallest]);
+    }
 }
 
 int main()
 {
     {
-        std::vector<int> values{7, 2, 9, 1, 5};
+        std::vector<int> values{7, 2, 9, 1, 5, 0, 11, 100, -5, 999, 0, 1};
         sortArray(values);
-        assert((values == std::vector<int>{1, 2, 5, 7, 9}));
+        assert((values == std::vector<int>{-5, 0, 0, 1, 1, 2, 5, 7, 9, 11, 100, 999}));
     }
 
     {
@@ -48,6 +65,8 @@ int main()
         sortArray(values);
         assert((values == std::vector<int>{1, 2, 3, 4, 5}));
     }
+
+    std::cout << "All passed\n";
 
     return 0;
 }
