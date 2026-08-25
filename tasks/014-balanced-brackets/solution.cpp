f7d2bd65d98504e72eb75d903a8a665d6f9a49d0
zelinsky alexander <34163821+zelinsky-alexander@users.noreply.github.com>
2026-08-25 11:37:05 +0300

    Update solution.cpp

--- tasks/014-balanced-brackets/solution.cpp
+++ tasks/014-balanced-brackets/solution.cpp
@@ -1,9 +1,36 @@
 #include <cassert>
 #include <string_view>
+#include <stack>
 
 bool hasBalancedBrackets(std::string_view text)
 {
-    return false;
+    std::stack<char> stk;
+    for (size_t i = 0; i < text.size(); ++i) {
+      char c = text[i];
+      switch(c) {
+        case '(':
+        case '[':
+        case '{':
+          stk.push(c);
+          break;
+        case ')':
+          if (stk.empty() || stk.top() != '(') return false;
+          stk.pop();
+          break;
+        case ']':
+          if (stk.empty() || stk.top () != '[') return false;
+          stk.pop();
+          break;
+        case '}':
+          if (stk.empty() || stk.top() != '{') return false;
+          stk.pop();
+  break;
+
+        default:
+          break;
+       }
+    }
+    return stk.empty();
 }
 
 int main()
