9ba91d1c17f840545b992f40b4a8c39b36b81707
Alex Zelinsky <zelinsky.alexander@gmail.com>
2026-08-26 12:31:33 +0300

    binary search

--- tasks/020-binary-search/solution.cpp
+++ tasks/020-binary-search/solution.cpp
@@ -2,8 +2,29 @@
 #include <cstddef>
 #include <optional>
 #include <vector>
+#include <iostream>
 
-std::optional<std::size_t> binarySearch(const std::vector<int>& values, int target);
+std::optional<std::size_t> binarySearch(const std::vector<int>& values, int target)
+{
+    size_t sz = values.size();
+    std::cout << "size: " << sz << " target:" << target << std::endl;
+
+    size_t left = 0;
+    size_t right = sz;
+
+    while (left < right) {
+        size_t mid = left + (right - left)/2;
+        std::cout << "->" << mid << std::endl;
+        int v = values[mid];
+        if (target == v) return mid;
+        if (target > v) 
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    return std::nullopt;
+}
 
 int main()
 {
@@ -15,6 +36,7 @@ int main()
     assert(binarySearch({1, 3, 5, 7, 9}, 9) == std::optional<std::size_t>{4});
     assert(binarySearch({1, 3, 5, 7, 9}, 4) == std::nullopt);
     assert(binarySearch({-10, -3, 0, 4, 8, 12}, -3) == std::optional<std::size_t>{1});
+    std::cout << "all tests passed" << std::endl;
 
     return 0;
 }
